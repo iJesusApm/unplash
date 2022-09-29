1052e592b7a3dc7c46d7941c3456a4886323aa09
iJesusApm <jesusapm14@gmail.com>
2022-09-29 05:24:20 -0400

    core: rename project

--- ios/unplashTests/unplashTests.m
+++ ios/unplashTests/unplashTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface UsahiTests : XCTestCase
+@interface unplashTests : XCTestCase
 
 @end
 
-@implementation UsahiTests
+@implementation unplashTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
--- src/hooks/useFetchPost.js
+++ src/hooks/useFetchPost.js
@@ -1,5 +1,5 @@
 import React, {createContext, useState, useEffect} from 'react';
-import {unsplash} from '../services/api';
+import {unsplash} from '../services/Api';
 
 // create context
 const PostContext = createContext();
